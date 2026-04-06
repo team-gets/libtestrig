d6e873ed930d9d6923e373c35aa21b3052cb91c0
JC Luna <jc.lunaa@protonmail.com>
2026-04-06 09:10:07 -0500

    parse flags

--- src/args.c
+++ src/args.c
@@ -1,12 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "args.h"
+
+static void die_invalid_arg(const char* arg) {
+	printf("error: invalid argument \"%s\"\n", arg);
+	exit(-1);
+}
 
 int is_flag(const char* arg) { return (strstr(arg, "--") == NULL) ? 0 : 1; }
 int is_opt(const char* arg) {
 	char* subarg;
 	if ((subarg = strstr(arg, "-"))) {
-		return (strstr(subarg, "-") == NULL) ? 0 : 1;
+		return (strstr(subarg, "-")) ? 1 : 0;
+	}
+	else {
+		return 0;
+	}
+}
+
+int parse_flag(const char* flag, struct parsed_args* parsed) {
+	const char* name = strstr(flag, "--");
+	
+	if (strncmp("daemon", name, 7) == 0) {
+		parsed->action = ACTION_DAEMON;
+		return 1;
+	}
+	else if (strncmp("detach", name, 7) == 0) {
+		parsed->mode = CLI_MODE_DETACHED;
+		return 1;
+	}
+	else if (strncmp("verbose", name, 8) == 0) {
+		parsed->verbosity = 1;
+		return 1;
 	}
 	else {
 		return 0;
@@ -14,18 +40,26 @@ int is_opt(const char* arg) {
 }
 
 void parse_args(int argc, char** argv, struct parsed_args* parsed) {
+	memset(parsed, 0, sizeof(struct parsed_args));
+	parsed->mode = CLI_MODE_CMD;
+	parsed->action = ACTION_HELP;
+
 	for (unsigned int i = 1; i < (unsigned int)argc; i++) {
-		int is_a_flag = is_flag(argv[i]);
-		int is_an_opt = is_opt(argv[i]);
+		char* arg = argv[i];
+		int is_a_flag = is_flag(arg);
+		int is_an_opt = is_opt(arg);
+		int result;
 		
 		if (is_a_flag) {
-
+			result = parse_flag(arg, parsed);
 		}
 		else if (is_an_opt) {
-
+			result = 1;
 		}
 		else {
-
+			result = 1;
 		}
+
+		if (!result) { die_invalid_arg(arg); }
 	}
 }
--- src/args.h
+++ src/args.h
@@ -1,11 +1,14 @@
 #pragma once
 
+#include <stdint.h>
+
 enum cli_mode {
 	CLI_MODE_CMD,
 	CLI_MODE_DETACHED
 };
 
 enum cli_action {
+	ACTION_HELP,
 	ACTION_IDENT,
 	ACTION_DAEMON,
 	ACTION_OPEN,
@@ -16,8 +19,11 @@ enum cli_action {
 struct parsed_args {
 	enum cli_mode mode;
 	enum cli_action action;
+	uint8_t verbosity;
 };
 
 int is_flag(const char* arg);
 int is_opt(const char* arg);
+
+int parse_flag(const char* flag, struct parsed_args* parsed);
 void parse_args(int argc, char** argv, struct parsed_args* parsed);
