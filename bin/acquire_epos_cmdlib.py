6220050d838d9b7050233493ccfca2b4c8c98c4f
JC Luna <jc.lunaa@protonmail.com>
2026-04-08 16:04:15 -0500

    Acquire binaries on CI

---
+++ bin/acquire_epos_cmdlib.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+
+import os
+import platform
+import shutil
+import subprocess
+
+from os.path import join, abspath
+dirs = {
+    "repo_root": abspath(join(__file__, "..", "..")),
+    "lib_dir": abspath(join(__file__, ".."))
+}
+
+listing_link = "https://www.maxongroup.us/maxon/view/product/control/Positionierung/375711"
+windows_link = "https://www.maxongroup.us/medias/sys_master/root/9443687104542/EPOS-Windows-DLL-En.zip"
+linux_link = "https://www.maxongroup.us/medias/sys_master/root/9443687202846/EPOS-Linux-Library-En.zip"
+
+def get_mfg(arch: str) -> str:
+    return "intel" if "x86" in arch else "arm"
+
+def win_process_contents(unzipped_dir: str|os.PathLike, arch: str) -> int:
+    orig_wd: os.PathLike = os.curdir
+    my_mfg = get_mfg(arch)
+    os.chdir(unzipped_dir)
+
+    return 0
+
+def linux_process_contents(unzipped_dir: str|os.PathLike, arch: str) -> int:
+    orig_wd: os.PathLike = os.curdir
+    my_mfg = get_mfg(arch)
+    os.chdir(join(unzipped_dir, "lib", my_mfg, arch))
+
+    return 0
+
+def attempt_dl(link: str, outf: str|os.PathLike) -> int:
+    try:
+        lib_get = subprocess.run([f"curl \"{link}\" --output {outf}"], shell=True)
+
+    except Exception as excepter:
+        print(excepter)
+        return 1
+
+    return 0
+
+if __name__ == "__main__":
+    i_am = platform.system()
+
+    os.makedirs(dirs["lib_dir"], exist_ok=True)
+
+    archive_name = join(dirs["lib_dir"], "libepos.zip")
+    attempt_code: int
+    arch: str = "x86_64"
+    libname: str = ""
+    other_libname: str = ""
+    dirname: str = ""
+
+    show_eula = input("The binary to install is property of maxon group. " \
+                      "By using this tool, you agree to the terms laid out by the EULA " \
+                      "that is included inside of the archive.")
+
+    if i_am == "Windows":
+        attempt_code = attempt_dl(windows_link, archive_name)
+        libname = "EposCmd.dll"
+        other_libname = "EposCmd.lib"
+        dirname = "Microsoft Visual C++"
+
+    elif i_am == "Linux":
+        attempt_code = attempt_dl(linux_link, archive_name)
+        libname = "libEposCmd.so.6.8.1.0"
+        dirname = arch
+
+    if (attempt_code != 0):
+        print(f"Please visit {listing_link} for a manual download.")
+        exit(1)
+    
+    print("Done downloading!")
+    print("Unzipping contents...") 
+
+    wherego = join(dirs["lib_dir"], "epos_temp")
+    shutil.unpack_archive(archive_name, wherego)
+
+    print("Processing contents...")
+
+    for (root, dirnames, files) in os.walk(wherego):
+        for f in files:
+            if "EULA.txt" in f:
+                os.rename(join(root, f), join(dirs["lib_dir"], f))
+                continue
+
+            if (not dirname in root):
+                continue
+            if (libname in f or (other_libname != "" and other_libname in f)):
+                os.rename(join(root, f), join(dirs["lib_dir"], f))
+
+    shutil.rmtree(wherego)
+    os.remove(archive_name)
+
+    where_eula = join(dirs["lib_dir"], "EULA.txt")
+    print(f"The following can be found at {where_eula}")
+
+    with open(where_eula, "r") as f:
+        for line in f.readlines():
+            print(line)
+
+    print(f"Done! Make sure to read the maxon EULA at {where_eula}")
+    exit(0)
