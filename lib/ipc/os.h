6449d7697666e655d1f5412ff26fbebd00986130
JC Luna <jc.lunaa@protonmail.com>
2026-04-22 15:10:48 -0500

    Move new perror out of ifdef

--- lib/ipc/os.h
+++ lib/ipc/os.h
@@ -19,10 +19,7 @@ extern "C" {
 TESTRIG_API int vscl_get_sock_destination(char* dest);
 TESTRIG_API int vscl_make_new_proc(const char* prog, const char* args);
 TESTRIG_API void vscl_sleep(uint32_t s);
-
-#ifdef _WIN32
-TESTRIG_API void vscl_os_perror(const TCHAR* msg);
-#endif
+TESTRIG_API void vscl_os_perror(const char* msg);
 
 #ifdef __cplusplus
 } // extern "C"
