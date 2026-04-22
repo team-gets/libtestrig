7948f2edbff4a53018395c1d93a55df7fd06ee95
JC Luna <jc.lunaa@protonmail.com>
2026-04-22 10:27:59 -0500

    Test the proc spawner

--- test/ipc/sock_communicating.c
+++ test/ipc/sock_communicating.c
@@ -1,3 +1,16 @@
+#include "ipc/sock.h"
+#include "ipc/os.h"
+#include <stdio.h>
+
 int main(int argc, char** argv) {
+    printf("This was invoked with the following command line:\n");
+    printf("\t%s %s\n", argv[0], argv[1]);
+
+    if (strncmp(argv[1], "serve", 6) == 0) {
+        int retstat = vscl_make_new_proc(argv[0], "child");
+        if (retstat == -1) { fprintf(stderr, "Failure to launch\n"); return retstat; }
+        else { printf("New process was spawned with PID %i\n", retstat); }
+    }
+
     return 0;
 }
